b45048992f37e9e4d380fc67d31f80ecb6919201
Dan Borufka <d.borufka@pjureisobar.com>
2015-08-12 12:27:36 +0200

    Cleanup

--- deploy.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	echo `git add . --all 2>&1`; 
-	$stamp = date('d.m.Y') . ' at ' . date('h:m') . ' and ' . date('s') . ' seconds';
-	echo shell_exec('git commit -m "file changed on ' . $stamp . '" 2>&1'); 
-	echo `git push https://danborufka:g1t579#0a@github.com/danborufka/danborufka.github.io.git 2>&1`; 
-?>
