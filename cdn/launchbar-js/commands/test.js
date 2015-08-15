a3cd24ab3b963b98528a9be63849b8f1bc1d5385
Dan Borufka <d.borufka@pjureisobar.com>
2015-08-15 16:31:13 +0200

    file changed on 15.08.2015 at 04:08 and 13 seconds

--- cdn/launchbar-js/commands/test.js
+++ cdn/launchbar-js/commands/test.js
@@ -1,1 +1,1 @@
-/* Sorry, ressource test.js not found. */
+LAUNCHBAR.install({ commands: { test: function() { LAUNCHBAR.chain({ commands: { free: function(){ alert('Freedom!');} }, labels: { free: 'The Braveheart Effect' } }); }, abc: function(){ alert('abcdefghijk…');} }, labels: { test: 'Simpler Test', abc: 'Alphabet' } });
