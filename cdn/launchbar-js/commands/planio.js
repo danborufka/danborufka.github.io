cbea1a6c203f0a94343df44f791554aa96acd575
Dan Borufka <d.borufka@pjureisobar.com>
2015-09-01 14:37:34 +0200

    file changed on 01.09.2015 at 02:09 and 34 seconds

--- cdn/launchbar-js/commands/planio.js
+++ cdn/launchbar-js/commands/planio.js
@@ -1,1 +1,1 @@
-$(".updated_on") .after('<td><input class="lb_done" type="checkbox" /></i></td>'); $(document) .on('click', '.lb_done', function() { alert('Speicheeeern!'); });
+$(".updated_on") .after('<td><input class="lb_done" type="checkbox" /></i></td>'); $(document) .on('click', '.lb_done', function() { var id=0; console.log($(this).parent()); console.log($(this).parentsUntil('tr')); alert('Speicheeeern von Aufgabe #'+id+' !'); });
