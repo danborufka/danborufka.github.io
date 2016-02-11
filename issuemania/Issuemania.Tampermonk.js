caf13fe69fbf510229f9ff02a5ad6f482c13bb97
Dan Borufka <thedan@dropz.io>
2016-02-11 13:20:37 +0100

    Create Issuemania.Tampermonk.js

---
+++ issuemania/Issuemania.Tampermonk.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         GH Issuemania
+// @namespace    http://dropz.io/
+// @version      0.1
+// @description  free all manatees worldwide from slavery using this awesome tool
+// @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js
+// @require      file:///Users/danborufka/dev/gitissues/issuemania.js
+// @author       Dan Borufka
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_getResourceText
+// @grant        GM_getResourceURL
+// @grant        GM_addStyle
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
