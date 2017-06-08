d0c1abafb2acab36ca3c80fc15e5850f91f00088
Dan Borufka <dan@sirenio.com>
2017-06-08 14:28:06 +0200

    Adding file watcher to grunt tasks for faster development

--- protos/letters/Gruntfile.js
+++ protos/letters/Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
+
     sass: {
         editor: {
             src: 'css/source/Danimator.editor.scss',
@@ -39,14 +40,26 @@ module.exports = function(grunt) {
         },
         target: {
             files: {
-                'js/Danimator.min.js': ['js/Danimator.js']//,
-                //'js/Danimator.editor.min.js': ['js/Danimator.editor.js']
+                'js/Danimator.min.js': ['js/Danimator.js'],
+                'js/game.min.js': ['js/source/game.js']
+                //,'js/Danimator.editor.min.js': ['js/Danimator.editor.js']
             }
         }
+    },
+    watch: {
+      scripts: {
+        files: ['js/source/*/*.js', 'js/source/*.js'],
+        tasks: ['concat:main', 'concat:editor', 'uglify']
+      },
+      styles: {
+        files: ['css/source/*.scss'],
+        tasks: ['sass', 'concat:styles', 'cssmin']
+      }
     }
+
   });
 
   require('load-grunt-tasks')(grunt);
-  grunt.registerTask('default', ['sass', 'concat', /*'cssmin',*/ 'uglify']);
+  grunt.registerTask('default', 'watch');
 
 };
