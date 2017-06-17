ac185402362138b4a12cf256df9f54e1e38a842a
Dan Borufka <dan@sirenio.com>
2017-06-17 14:55:50 +0200

    Moving Danimator out of repo -> into its own

--- protos/letters/Gruntfile.js
+++ protos/letters/Gruntfile.js
@@ -5,30 +5,14 @@ module.exports = function(grunt) {
 
     sass: {
         editor: {
-            src: 'css/source/Danimator.editor.scss',
-            dest: 'css/source/Danimator.editor.css'
-        }
-    },
-    concat: {
-        main: {
-            options: { separator: ';' },
-            src: 'js/source/*.js',
-            dest: 'js/Danimator.js'
-        },
-        editor: {
-            options: { separator: ';' },
-            src: 'js/source/editor/*.js',
-            dest: 'js/Danimator.editor.js'
-        },
-        styles: {
-            src: 'css/source/*.css',
-            dest: 'css/Danimator.editor.css'
+            src: 'css/source/game.scss',
+            dest: 'css/game.css'
         }
     },
     cssmin: {
         editor: {
-            src: 'css/Danimator.editor.css',
-            dest:'css/Danimator.editor.min.css'
+            src: 'css/game.css',
+            dest:'css/game.min.css'
         }
     },
     uglify: {
@@ -40,30 +24,24 @@ module.exports = function(grunt) {
         },
         target: {
             files: {
-                'js/Danimator.min.js': ['js/Danimator.js'],
                 'js/game.min.js': ['js/game.js']
-                //,'js/Danimator.editor.min.js': ['js/Danimator.editor.js']
             }
         }
     },
     watch: {
-      scripts: {
-        files: ['js/source/*/*.js', 'js/source/*.js'],
-        tasks: ['concat:main', 'concat:editor', 'uglify']
-      },
-      games: {
+      game: {
         files: ['js/game.js'],
-        tasks: ['concat:main', 'concat:editor', 'uglify']
+        tasks: ['uglify']
       },
       styles: {
         files: ['css/source/*.scss'],
-        tasks: ['sass', 'concat:styles', 'cssmin']
+        tasks: ['sass','cssmin']
       }
     }
 
   });
 
   require('load-grunt-tasks')(grunt);
-  grunt.registerTask('default', 'watch');
+  grunt.registerTask('default', ['uglify', 'sass', 'cssmin', 'watch']);
 
 };
